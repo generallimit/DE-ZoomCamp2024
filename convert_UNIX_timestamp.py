7ef67d902e022e96e5f1963831bf66a46077c194
Jeff Patton <patton.jeffery@gmail.com>
2024-02-07 21:36:06 -0700

    Homework 3 and some repo changes

---
+++ convert_UNIX_timestamp.py
@@ -0,0 +1,13 @@
+# Convert UNIX timestamp; Python
+
+
+import datetime
+timestamp_with_ms = 1626325266384
+dt = datetime.datetime.fromtimestamp(timestamp_with_ms / 1000)
+formatted_time = dt.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+print(formatted_time)
+
+# 2021-07-15 07:01:06.384
+
+1640996061000000000
+
